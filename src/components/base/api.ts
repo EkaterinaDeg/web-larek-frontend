917e7b443939e01520bdc8bb8ef89945ceb59461
Ekaterina Degtyariova <ekaterinadegtyariova@yandex.ru>
2025-07-23 13:30:11 +0500

    первый вариант восьмого спринта

--- src/components/base/api.ts
+++ src/components/base/api.ts
@@ -1,42 +1,53 @@
 export type ApiListResponse<Type> = {
-    total: number,
-    items: Type[]
+  total: number;
+  items: Type[];
 };
 
 export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
 
 export class Api {
-    readonly baseUrl: string;
-    protected options: RequestInit;
+  readonly baseUrl: string;
+  protected options: RequestInit;
 
-    constructor(baseUrl: string, options: RequestInit = {}) {
-        this.baseUrl = baseUrl;
-        this.options = {
-            headers: {
-                'Content-Type': 'application/json',
-                ...(options.headers as object ?? {})
-            }
-        };
-    }
+  constructor(baseUrl: string, options: RequestInit = {}) {
+    this.baseUrl = baseUrl;
 
-    protected handleResponse(response: Response): Promise<object> {
-        if (response.ok) return response.json();
-        else return response.json()
-            .then(data => Promise.reject(data.error ?? response.statusText));
-    }
+    // Безопасное объединение headers
+    const baseHeaders = {
+      'Content-Type': 'application/json',
+    };
 
-    get(uri: string) {
-        return fetch(this.baseUrl + uri, {
-            ...this.options,
-            method: 'GET'
-        }).then(this.handleResponse);
-    }
+    this.options = {
+      ...options,
+      headers: {
+        ...baseHeaders,
+        ...(options.headers ?? {}),
+      },
+    };
+  }
 
-    post(uri: string, data: object, method: ApiPostMethods = 'POST') {
-        return fetch(this.baseUrl + uri, {
-            ...this.options,
-            method,
-            body: JSON.stringify(data)
-        }).then(this.handleResponse);
+  protected async handleResponse<T = any>(response: Response): Promise<T> {
+    const data = await response.json().catch(() => ({}));
+    if (response.ok) {
+      return data;
+    } else {
+      const errorMessage = data.error || response.statusText || 'Unknown error';
+      return Promise.reject(new Error(errorMessage));
     }
+  }
+
+  get<T = any>(uri: string): Promise<T> {
+    return fetch(this.baseUrl + uri, {
+      ...this.options,
+      method: 'GET',
+    }).then(this.handleResponse);
+  }
+
+  post<T = any>(uri: string, data: object, method: ApiPostMethods = 'POST'): Promise<T> {
+    return fetch(this.baseUrl + uri, {
+      ...this.options,
+      method,
+      body: JSON.stringify(data),
+    }).then(this.handleResponse);
+}
 }
